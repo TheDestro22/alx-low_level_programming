7c13db2c3d82da90a9989d8d3229326e68e3aedf
Mohamed Ehab <messk8@gmail.com>
2023-09-12 16:36:34 -0700

    fixing an error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,11 @@
  * Description : A file that prints the phrase
  *	(_putchar)
  *
- * Return : 0 (success)
+ * Return: 0 (success)
 */
 
 int main(void)
 {
 	write(1, "_putchar\n", 9);
-	return 0;
+	return (0);
 }
