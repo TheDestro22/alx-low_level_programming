1fe62c515458d11f69bd4dd3d3227900c5265ef8
Mohamed Ehab <messk8@gmail.com>
2023-09-12 17:20:17 -0700

    fixing an error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,15 @@
  * Return: 0 (success)
 */
 
+void print_alphabet(void)
+{
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+        _putchar(letter);
+        }
+        _putchar('\n');
+}
+
 int main(void)
 {
 	void print_alphabet(void);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,12 +4,5 @@
 #include <unistd.h>
 int _putchar(char c);
 void print_alphabet(void)
-{
-        for(char letter = 'a'; letter <= 'z'; letter++)
-        {
-        _putchar(letter);
-        }
-        _putchar('\n');
-}
 
 #endif
