d62a2da2f44437c85bdf335e1a788baea7c46d0c
Mohamed Ehab <messk8@gmail.com>
2023-09-12 17:15:52 -0700

    fixing an error

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,13 @@
 
 #include <unistd.h>
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+        _putchar(letter);
+        }
+        _putchar('\n');
+}
 
 #endif
