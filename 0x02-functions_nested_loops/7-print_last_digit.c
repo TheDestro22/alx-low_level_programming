100380156f5bc40033bf2ac8f9bdd73cbf978186
Mohamed Ehab <messk8@gmail.com>
2023-09-13 16:31:53 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ *
+ * @n: to be checked
+ *
+ * Return: the last digit of n
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
+
