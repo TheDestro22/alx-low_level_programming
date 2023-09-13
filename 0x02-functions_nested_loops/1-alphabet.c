5003809d3c23fbe64af163883e52a15186e5bea4
Mohamed Ehab <messk8@gmail.com>
2023-09-12 17:30:26 -0700

    fixing an error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,6 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
  */
-void print_alphabet(void)
-{
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
-}
 
 int main(void)
 {
