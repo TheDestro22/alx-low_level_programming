955ed98293e32b83d3cf13e576f4722724cfa45a
Mohamed Ehab <messk8@gmail.com>
2023-09-13 16:55:14 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@
 
 int print_last_digit(int n)
 {
-	if (n > 10)
+	if (n > 10 || n < 0)
 	{
-	n = n % 10;
-	_putchar('0' + n);
-	return (n);
+		n = n % 10;
+		_putchar('0' + n);
+		return (n);
 	}
 	else
 	{
