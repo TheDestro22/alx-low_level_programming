e79c9c819543c89eb8102f22b66d7507a416ae2f
Mohamed Ehab <messk8@gmail.com>
2023-09-14 13:48:04 -0700

    done

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: false or ture validation
+ *
+ * Return: 0 (success)
+*/
+
+void positive_or_negative(int i);
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i == 0)
+		printf("%i is zero\n", i);
+	else
+		printf("%i is negative\n", i);
+	return (0);
+}
