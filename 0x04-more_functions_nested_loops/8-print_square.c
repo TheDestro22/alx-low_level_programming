47375a44ca7c12cae7807f83cc43f155281bc04c
Mohamed Ehab <messk8@gmail.com>
2023-09-14 19:59:52 -0700

    done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,16 +6,27 @@
  * @size: is the size of the square
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (row = 1; row <= size; row++)
 	{
 		for (column = 1; column <= size; column++)
+		{
 			_putchar('#');
+			if (column < size)
+				_putchar(' ');
+		}
 		_putchar('\n');
 	}
 }
+
