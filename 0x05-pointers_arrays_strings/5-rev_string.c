3c468f1b97e421f20682e6c037df5025a5872ae1
Mohamed Ehab <messk8@gmail.com>
2023-09-18 15:36:35 -0700

    fixing an error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * rev_string - prints a string in reverse, followed by a new line
  *
  * @s: pointer for a character
  *
@@ -15,7 +15,7 @@ void rev_string(char *s)
 		_putchar(*s);
 		s++;
 	}
-
+	_putchar('\n');
 	s--;
 
 	while (*s > 0)
