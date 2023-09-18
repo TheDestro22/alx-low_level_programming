360fdf1ca37c8644e92c41f701ae3f9f24c0d342
Mohamed Ehab <messk8@gmail.com>
2023-09-18 13:56:28 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ *
+ * @str: pointer for a character
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+
+{
+
+for (; *str != '\0'; str++)
+	_putchar(*str);
+-putchar('\n');
+
+}
