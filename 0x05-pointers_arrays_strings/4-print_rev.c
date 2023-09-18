4093118cd54d2eb94a28bdc86aef28a03507b0da
Mohamed Ehab <messk8@gmail.com>
2023-09-18 14:24:29 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,17 @@
 
 void print_rev(char *s)
 {
+	while (*s != '\0')
+	{
+		s++;
+	}
 
-*s = '\0';
+	s--;
 
-for (; *s != 0; s--)
-	_putchar(*s);
-_putchar('\n');
-
+	while (*s <= 0)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
