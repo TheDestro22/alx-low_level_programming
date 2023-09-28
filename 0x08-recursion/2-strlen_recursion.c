60d20d6b2fb98400f077c1b3ecbdd7389f7fd6f6
Mohamed Ehab <messk8@gmail.com>
2023-09-28 14:47:08 -0700

    Let's see what error will appear this time

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ *
+ * @s: recieves the string input
+ *
+ * Return: the length of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int counter = 0;
+
+	if (*s == '\0')
+	{
+		return (counter);
+	}
+	else
+	{
+		counter++;
+		_strlen_recursion(s + 1);
+		return (counter);
+	}
+}
