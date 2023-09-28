65fba508d9eddcbfe93f576d356a33c74d3bfdda
Mohamed Ehab <messk8@gmail.com>
2023-09-28 14:32:44 -0700

    I can do that all day if you want me to

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - pritns a string in reverse,
+ * followed by a new line
+ *
+ * @s: recieves the string input
+ *
+ * Return: Nothing
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		 _print_rev_recursion(s + 1);
+		 _putchar(*s);
+	}
+}
