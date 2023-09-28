03b986f44a6de457b4732ff67b4a92cd312fee51
Mohamed Ehab <messk8@gmail.com>
2023-09-28 15:52:17 -0700

    if it worked , I'll be amazed

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - returns the lenght of a string
+ *
+ * @n: recieves the int input
+ *
+ * Return: factorial of the input
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		n *= factorial(n - 1);
+	}
+	return (n);
+}
