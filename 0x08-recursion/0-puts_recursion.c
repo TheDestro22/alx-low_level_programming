0401711b3abf56c31e34c3bc1bd6c6291b10896c
Mohamed Ehab <messk8@gmail.com>
2023-09-28 14:22:38 -0700

    How do you like that ?

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,7 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	_puts_recursion(*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
