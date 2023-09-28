4c167d8ee5539efd2e2662306aecbecd91758e45
Mohamed Ehab <messk8@gmail.com>
2023-09-28 16:15:47 -0700

    hope it's done , cause if it is not , that would be problomatic

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the lenght of a string
+ *
+ * @x: recieves the int input
+ * @y: rectieves the int input
+ *
+ * Return: @x raised to the power of @y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1))
+}
